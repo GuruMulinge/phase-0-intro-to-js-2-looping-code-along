5d41d1810a534787c5608a18acd16e2ad2091e7a
Anthony Mulinge <anthony.mulinge@student.moringaschool.com>
2023-03-17 15:54:35 +0300

    Assignment Commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// writeCards function
+function writeCards(namesArray, event) {
+    let thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // countDown function
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+
+console.log(countDown(10));
+
+console.log(writeCards(["Simon Peter", "Anthony Mulinge"], "birthday"));
